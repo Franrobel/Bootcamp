af623b936009161e42bdd874e921be103dd58c3c
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2024-07-24 21:28:33 +0200

    adding components

--- parte1/courseinfo/src/App.js
+++ parte1/courseinfo/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header"
+import Content from "./components/Content/Content"
+import Total from "./components/Total/Total"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+   
+const courseContent = { 
+  part1 : {title: 'Fundamentals of React', exercises: 10},
+  part2 : {title: 'Using props to pass data', exercises: 7},
+  part3 : {title: 'State of a component', exercises: 14}
+};
+
+const sumExercisesCourse = courseContent.part1.exercises + courseContent.part2.exercises + courseContent.part3.exercises;
+ 
+return (
+    <div>
+      <Header course = 'Half Stack application development'/>
+      <Content part = {courseContent.part1.title} exercises = {courseContent.part1.exercises} />
+      <Content part = {courseContent.part2.title} exercises = {courseContent.part2.exercises}/>
+      <Content part = {courseContent.part3.title} exercises = {courseContent.part3.exercises}/>
+      <Total total={sumExercisesCourse}/>  
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
