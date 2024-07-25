40537e218ba530c2f91549dcbec191e92be6baa7
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2024-07-25 14:30:36 +0200

    sum total exercises from array

--- parte1/courseinfo/src/App.js
+++ parte1/courseinfo/src/App.js
@@ -3,22 +3,20 @@ import Content from "./components/Content/Content"
 import Total from "./components/Total/Total"
 
 const App = () => {
-   
-const courseContent = { 
-  part1 : {title: 'Fundamentals of React', exercises: 10},
-  part2 : {title: 'Using props to pass data', exercises: 7},
-  part3 : {title: 'State of a component', exercises: 14}
-};
 
-const sumExercisesCourse = courseContent.part1.exercises + courseContent.part2.exercises + courseContent.part3.exercises;
- 
-return (
+  const courseContent = [
+    { title: 'Fundamentals of React', exercises: 10 },
+    { title: 'Using props to pass data', exercises: 7 },
+    { title: 'State of a component', exercises: 14 }
+  ];
+
+  const sumExercisesCourse = courseContent.reduce((total, course) => total + course.exercises, 0);
+
+  return (
     <div>
-      <Header course = 'Half Stack application development'/>
-      <Content part = {courseContent.part1.title} exercises = {courseContent.part1.exercises} />
-      <Content part = {courseContent.part2.title} exercises = {courseContent.part2.exercises}/>
-      <Content part = {courseContent.part3.title} exercises = {courseContent.part3.exercises}/>
-      <Total total={sumExercisesCourse}/>  
+      <Header course='Half Stack application development' />
+      <Content courseInfo={courseContent} />
+      <Total total={sumExercisesCourse} />
     </div>
   )
 }
